4fc5a52e07aff96a770c924d49609d75d13b330f
alexispanelo14-oss <alexispanelo14@gmail.com>
2026-09-10 17:26:50 -0300

    Add files via upload

---
+++ matches.js
@@ -0,0 +1,6 @@
+// DEMO. En la siguiente fase se conecta una API deportiva para actualizar automáticamente.
+const MATCHES=[
+{id:"d1",competition:"CHAMPIONS LEAGUE",type:"champions",date:"Próximo partido",home:"Real Madrid",away:"Manchester City",flag:"🇪🇺"},
+{id:"d2",competition:"COPA LIBERTADORES",type:"libertadores",date:"Próximo partido",home:"Flamengo",away:"River Plate",flag:"🏆"},
+{id:"d3",competition:"COPA SUDAMERICANA",type:"sudamericana",date:"Próximo partido",home:"Lanús",away:"Atlético Mineiro",flag:"🏆"},
+{id:"d4",competition:"AMISTOSO DE SELECCIONES",type:"amistosos",date:"Próximo partido",home:"Argentina",away:"Brasil",flag:"🌎"}];
